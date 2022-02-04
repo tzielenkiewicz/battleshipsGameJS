c5babba97f844971f78448c115a829b3217effb8
tzielenkiewicz <tomasz.jacek.zielenkiewicz@gmail.com>
2022-02-04 21:52:13 +0100

    you can input ships even smarter

--- js/battleships.js
+++ js/battleships.js
@@ -9,8 +9,7 @@ function startTheGame() {
 		for(j=1; j<11; j++) {
 			y[j].id='p'+(i-1)+(j-1);
 			y[j].onclick = function() {
-				if (this.innerHTML=='') inputShip(this.id);
-				else alert('Wrong choice!');
+				inputShip(this.id);
 			};
 		}
 	}
@@ -22,8 +21,8 @@ function inputShip(id) {
 	let no1=parseInt(id[1]);
 	let no2=parseInt(id[2]);
 	let position=document.getElementById('vert').checked;
-	let location=null;
-	let no, n1, n2;
+	let location;
+	let no;
 	switch (position) {
 		case false: {
 			if (counter<5) {
@@ -31,25 +30,24 @@ function inputShip(id) {
 					
 					if(no2<6) no=no2+i;
 					else no=no2-i;
-					location=document.getElementById(id[0]+no1+no);
-					location.innerHTML='x';
+					document.getElementById(id[0]+no1+no).innerHTML='x';
 					
-					console.log(id[0]+no1+no);
 					counter++;
 				}
 			}
 			else if (counter<13) {
-				
-				for(i=0; i<4; i++) {
-					
-					if(no2<7) no=no2+i;
-					else no=no2-i;
-					location=document.getElementById(id[0]+no1+no);
-					location.innerHTML='x';
-					
-					console.log(id[0]+no1+no);
-					counter++;
+				if (checkLocation(id[0], no1, no2, position)==true){
+					for(i=0; i<4; i++) {
+
+						if(no2<7) no=no2+i;
+						else no=no2-i;
+						document.getElementById(id[0]+no1+no).innerHTML='x';
+
+						console.log(id[0]+no1+no);
+						counter++;
+					}
 				}
+				else alert('Wrong location!');
 			}
 			else alert('Too many ships!');
 		}
@@ -61,30 +59,72 @@ function inputShip(id) {
 					
 					if(no1<6) no=no1+i;
 					else no=no1-i;
-					location=document.getElementById(id[0]+no+no2);
-					location.innerHTML='x';
+					document.getElementById(id[0]+no+no2).innerHTML='x';
 					
-					console.log(id[0]+no+no2);
 					counter++;
 				}
 			}
 			else if (counter<13) {
-				
-				for(i=0; i<4; i++) {
-					
-					if(no1<7) no=no1+i;
-					else no=no1-i;
-					location=document.getElementById(id[0]+no+no2);
-					location.innerHTML='x';
-					
-					console.log(id[0]+no+no2);
-					counter++;
+				if (checkLocation(id[0], no1, no2, position)==true){
+					for(i=0; i<4; i++) {
+
+						if(no1<7) no=no1+i;
+						else no=no1-i;
+						document.getElementById(id[0]+no+no2).innerHTML='x';
+
+						console.log(id[0]+no+no2);
+						counter++;
+					}
 				}
+				else alert('Wrong location!');
 			}
 			else alert('Too many ships!');
 		}
 			break;
 	}
+}
+
+function checkLocation(letter, n1, n2, position) {
+	let correct=true;
+	let n=0;
+	let nBis=0;
+	switch (position) {
+		case false: {
+			for(j=-1; j<2; j++) {
+				nBis=n1+j;
+				if(nBis<0 || nBis>9) nBis=n1;
+				for(i=-1; i<5; i++) {
+					if(n2<7) n=n2+i;
+					else n=n2-i;
+					if(n<0 || n>9) n=n2;
+					if (document.getElementById(letter+nBis+n).innerHTML=='x') {
+						correct=false;
+						break;
+					}
+				}
+			}
+		}
+		break;
+		
+		case true: {
+			for(j=-1; j<2; j++) {
+				nBis=n2+j;
+				if(nBis<0 || nBis>9) nBis=n2;
+				for(i=-1; i<5; i++) {
+					if(n1<7) n=n1+i;
+					else n=n1-i;
+					if(n<0 || n>9) n=n1;
+					if (document.getElementById(letter+n+nBis).innerHTML=='x') {
+						correct=false;
+						
+					}
+				}
+			}
+		}
+		break;
+	}
+	
+	return correct;
 }
 
 
